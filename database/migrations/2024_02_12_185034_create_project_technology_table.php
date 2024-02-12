e44b7db1c45806f8c8f096df16772d400f8fd24b
lordevito <lollo.devito@hotmail.com>
2024-02-12 19:54:26 +0100

    feat: create migration technologies, creato model technology e creata migration per tabella pivot project_technology

--- database/migrations/2024_02_12_185034_create_project_technology_table.php
+++ database/migrations/2024_02_12_185034_create_project_technology_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')->references('id')->on('technologies')->cascadeOnDelete();
+            $table->primary(['project_id', 'technology_id']);
             $table->timestamps();
         });
     }
